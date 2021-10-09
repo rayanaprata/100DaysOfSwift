596237d17adea8fd192e4de531c5e805dd6195b8
Rayana Prata <50561896+rayanaprata@users.noreply.github.com>
2021-10-09 19:57:20 -0300

    range operators

--- Day 3/Day3.playground/Contents.swift
+++ Day 3/Day3.playground/Contents.swift
@@ -146,3 +146,26 @@ case "sunny":
 default:
     print("Enjoy your day!")
 }
+
+
+
+// range operators
+
+// Swift gives us two ways of making ranges: the ..< and ... operators. The half-open range operator, ..<, creates ranges up to but excluding the final value, and the closed range operator, ..., creates ranges up to and including the final value.
+
+// For example, the range 1..<5 contains the numbers 1, 2, 3, and 4, whereas the range 1...5 contains the numbers 1, 2, 3, 4, and 5.
+
+// Ranges are helpful with switch blocks, because you can use them for each of your cases. For example, if someone sat an exam we could print different messages depending on their score:
+
+let value = 84
+
+switch value {
+case 0..<50:
+    print("You failed badly.")
+case 50..<85:
+    print("You did OK.")
+default:
+    print("You did great!")
+}
+
+// As before, the default case must be there to ensure all possible values are covered.
