5950019e5bc3d17df26f70a87c6c65641f8218bb
Rayana Prata <50561896+rayanaprata@users.noreply.github.com>
2021-10-09 19:59:44 -0300

    operators and conditions summary

--- Day 3/Day3.playground/Contents.swift
+++ Day 3/Day3.playground/Contents.swift
@@ -169,3 +169,20 @@ default:
 }
 
 // As before, the default case must be there to ensure all possible values are covered.
+
+
+
+
+// operators and conditions summary
+
+// 1. Swift has operators for doing arithmetic and for comparison; they mostly work like you already know.
+
+// 2. There are compound variants of arithmetic operators that modify their variables in place: +=, -=, and so on.
+
+// 3. You can use if, else, and else if to run code based on the result of a condition.
+
+// 4. Swift has a ternary operator that combines a check with true and false code blocks. Although you might see it in other code, I wouldn’t recommend using it yourself.
+
+// 5. If you have multiple conditions using the same value, it’s often clearer to use switch instead.
+
+// 6. You can make ranges using ..< and ... depending on whether the last number should be excluded or included.
