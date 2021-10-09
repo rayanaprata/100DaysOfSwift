151f561b7d9bb98eb1186408389c5badd201eb19
Rayana Prata <50561896+rayanaprata@users.noreply.github.com>
2021-10-09 19:35:30 -0300

    arithmetic operators

---
+++ Day 3/Day3.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+// arithmetic operators
+
+let firstScore = 12
+let secondScore = 4
+
+let total = firstScore + secondScore
+let difference = firstScore - secondScore
+
+let product = firstScore * secondScore
+let divided = firstScore / secondScore
+
+let remainder = 13 % secondScore
+
+
