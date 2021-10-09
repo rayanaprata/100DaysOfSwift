b4a678c0be0717c9a8c2fdca2a17160879efd5d3
Rayana Prata <50561896+rayanaprata@users.noreply.github.com>
2021-10-09 19:48:25 -0300

    combining conditions

--- Day 3/Day3.playground/Contents.swift
+++ Day 3/Day3.playground/Contents.swift
@@ -84,3 +84,21 @@ if firstCard + secondCard == 2 {
 } else {
     print("Regular cards")
 }
+
+
+
+// combining conditions
+
+let age1 = 12
+let age2 = 21
+
+// && -> and
+if age1 > 18 && age2 > 18 {
+    print("Both are over 18")
+}
+
+// || -> or
+if age1 > 18 || age2 > 18 {
+    print("At least one is over 18")
+}
+
