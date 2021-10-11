39af60fb452045f92b0a2abcdf357c25422eab7a
Rayana Prata <50561896+rayanaprata@users.noreply.github.com>
2021-10-10 22:57:16 -0300

    for loops

---
+++ Day 4/Day4.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+// for loops
+
+let count = 1...10
+
+for number in count {
+    print("Number is \(number)")
+}
+
+let albums = ["Red", "1989", "Reputation"]
+
+for album in albums {
+    print("\(album) is on Apple Music")
+}
+
+// if you don’t use the constant that for loops give you, you should use an underscore instead so that Swift doesn’t create needless values:
+
+print("Players gonna ")
+
+for _ in 1...5 {
+    print("play")
+}
