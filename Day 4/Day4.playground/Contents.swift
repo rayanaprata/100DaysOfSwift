03a853d02feeab711fc23d3fde7d4bdd9f5d3218
Rayana Prata <50561896+rayanaprata@users.noreply.github.com>
2021-10-10 23:10:32 -0300

    exiting multiple loops

--- Day 4/Day4.playground/Contents.swift
+++ Day 4/Day4.playground/Contents.swift
@@ -68,3 +68,43 @@ while countDown >= 0 {
 }
 
 print("Blast off!")
+
+
+
+// exiting multiple loops
+
+// If you put a loop inside a loop it’s called a nested loop, and it’s not uncommon to want to break out of both the inner loop and the outer loop at the same time.
+
+// As an example, we could write some code to calculate the times tables from 1 through 10 like this:
+
+//for i in 1...10 {
+//    for j in 1...10 {
+//        let product = i * j
+//        print ("\(i) * \(j) is \(product)")
+//    }
+//}
+
+// If we wanted to exit part-way through we need to do two things. First, we give the outside loop a label, like this:
+
+//outerLoop: for i in 1...10 {
+//    for j in 1...10 {
+//        let product = i * j
+//        print ("\(i) * \(j) is \(product)")
+//    }
+//}
+
+// Second, add our condition inside the inner loop, then use break outerLoop to exit both loops at the same time:
+
+outerLoop: for i in 1...10 {
+    for j in 1...10 {
+        let product = i * j
+        print ("\(i) * \(j) is \(product)")
+
+        if product == 50 {
+            print("It's a bullseye!")
+            break outerLoop
+        }
+    }
+}
+
+// With a regular break, only the inner loop would be exited – the outer loop would continue where it left off.
