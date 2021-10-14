56c26746381f493e5112c1c6280e4591452a84d3
Rayana Prata <50561896+rayanaprata@users.noreply.github.com>
2021-10-14 17:37:10 -0300

    closures summary

--- Day 7/Day7.playground/Contents.swift
+++ Day 7/Day7.playground/Contents.swift
@@ -147,3 +147,19 @@ let res = travelCapturingValues()
 res("London")
 res("London")
 res("London")
+
+
+
+// closures summary
+
+// 1. You can assign closures to variables, then call them later on.
+
+// 2. Closures can accept parameters and return values, like regular functions.
+
+// 3. You can pass closures into functions as parameters, and those closures can have parameters of their own and a return value.
+
+// 4. If the last parameter to your function is a closure, you can use trailing closure syntax.
+
+// 5. Swift automatically provides shorthand parameter names like $0 and $1, but not everyone uses them.
+
+// 6. If you use external values inside your closures, they will be captured so the closure can refer to them later.
