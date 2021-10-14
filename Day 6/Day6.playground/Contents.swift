5a35b0fd5e23be8543445b816b2ad61eb9e224a1
Rayana Prata <50561896+rayanaprata@users.noreply.github.com>
2021-10-13 21:48:27 -0300

    accepting parameters in a closure

--- Day 6/Day6.playground/Contents.swift
+++ Day 6/Day6.playground/Contents.swift
@@ -15,3 +15,20 @@ let driving = {
 
 // that effectively creates a function without a name, and assigns that function to driving. you can now call driving() as if it were a regular function
 driving()
+
+
+
+// accepting parameters in a closure
+
+// when you create closures, they don't have a name or space to write any parameters that doesn’t mean they can’t accept parameters, just that they do so in a different way: they are listed inside the open braces
+
+// to make a closure accept parameters, list them inside parentheses just after the opening brace, then write in so that Swift knows the main body of the closure is starting
+
+let drivingWithParameters = { (place: String) in
+    print("I'm going to \(place) in my car")
+}
+
+// One of the differences between functions and closures is that you don’t use parameter labels when running closures. So, to call driving() now we’d write this
+drivingWithParameters("London")
+
+
