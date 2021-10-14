89ece62a119295f74e247f3ea41df312299f7493
Rayana Prata <50561896+rayanaprata@users.noreply.github.com>
2021-10-14 17:35:07 -0300

    capturing values

--- Day 7/Day7.playground/Contents.swift
+++ Day 7/Day7.playground/Contents.swift
@@ -112,3 +112,38 @@ result("London")
 
 
 
+// capturing values
+
+// If you use any external values inside your closure, Swift captures them – stores them alongside the closure, so they can be modified even if they don’t exist any more.
+
+// Right now we have a travel() function that returns a closure, and the returned closure accepts a string as its only parameter and returns nothing:
+//func travelCapturingValues() -> (String) -> Void {
+//    return {
+//        print("I'm going to \($0)")
+//    }
+//}
+
+// We can call travel() to get back the closure, then call that closure freely:
+//let result = travelCapturingValues()
+//result("London")
+
+// Closure capturing happens if we create values in travel() that get used inside the closure. For example, we might want to track how often the returned closure is called:
+
+func travelCapturingValues() -> (String) -> Void {
+    var counter = 1
+
+    return {
+        print("\(counter). I'm going to \($0)")
+        counter += 1
+    }
+}
+
+//Even though that counter variable was created inside travel(), it gets captured by the closure so it will still remain alive for that closure.
+
+//So, if we call result("London") multiple times, the counter will go up and up:
+
+let res = travelCapturingValues()
+
+res("London")
+res("London")
+res("London")
