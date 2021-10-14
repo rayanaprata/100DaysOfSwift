db752b7693946e474580544d100916c2f578db4d
Rayana Prata <50561896+rayanaprata@users.noreply.github.com>
2021-10-13 21:56:29 -0300

    closures as parameters

--- Day 6/Day6.playground/Contents.swift
+++ Day 6/Day6.playground/Contents.swift
@@ -48,3 +48,18 @@ print(message)
 
 
 
+// closures as parameters
+
+// because closures can be used like string and integers, you can pass them into functions
+
+// If we wanted to pass that closure into a function so it can be run inside that function, we would specify the parameter type as () -> Void. That means “accepts no parameters, and returns Void” – Swift’s way of saying “nothing”.
+
+// So, we can write a travel() function that accepts different kinds of traveling actions, and prints a message before and after:
+
+func travel(action: () -> Void) {
+    print("I'm getting ready to go.")
+    action()
+    print("I arrived!")
+}
+
+travel(action: driving)
