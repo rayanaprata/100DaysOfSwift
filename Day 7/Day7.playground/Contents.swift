8d71be5ebf77e16c9a61c044337d10e6627c3b21
Rayana Prata <50561896+rayanaprata@users.noreply.github.com>
2021-10-14 14:58:21 -0300

    using closures as parameters when they return values

--- Day 7/Day7.playground/Contents.swift
+++ Day 7/Day7.playground/Contents.swift
@@ -15,3 +15,24 @@ func travel(action: (String) -> Void) {
 travel { (place: String) in
     print("I'm going to \(place) in my car.")
 }
+
+
+
+// using closures as parameters when they return values
+
+// We’ve been using () -> Void to mean “accepts no parameters and returns nothing”
+
+// To demonstrate this, we can write a travel() function that accepts a closure as its only parameter, and that closure in turn accepts a string and returns a string:
+
+func eat(food: (String) -> String) {
+    print("I'm getting ready to eat.")
+    let description = food("Bread")
+    print(description)
+    print("I ate!")
+}
+
+// we call travel() using trailing closure syntax, our closure code is required to accept a string and return a string:
+
+eat { (kindOfFood: String) -> String in
+    return "I'm eating \(kindOfFood) in my house"
+}
