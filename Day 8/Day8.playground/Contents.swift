6f24622767b7afdf44f87870166221e0ff424e73
Rayana Prata <50561896+rayanaprata@users.noreply.github.com>
2021-10-24 20:51:19 -0300

    methods

--- Day 8/Day8.playground/Contents.swift
+++ Day 8/Day8.playground/Contents.swift
@@ -69,3 +69,16 @@ progress.amount = 80
 progress.amount = 100
 
 
+
+// methods -> functions inside structs
+
+struct City {
+    var population: Int
+    
+    func collectTaxes() -> Int {
+        return population * 1000
+    }
+}
+
+let london = City(population: 9_000_000)
+london.collectTaxes()
