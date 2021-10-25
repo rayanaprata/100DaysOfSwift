c2aa1ea19485acdeb1257b301c92f69db5128483
Rayana Prata <50561896+rayanaprata@users.noreply.github.com>
2021-10-24 21:33:46 -0300

    access control

--- Day 9/day9.playground/Contents.swift
+++ Day 9/day9.playground/Contents.swift
@@ -116,3 +116,55 @@ let ale = Student2(name: "Ale")
 // Because the classSize property belongs to the struct itself rather than instances of the struct, we need to read it using Student.classSize:
 
 print(Student2.classSize)
+
+
+
+// access control
+
+// Access control lets you restrict which code can use properties and methods. This is important because you might want to stop people reading a property directly, for example.
+
+// We could create a Person struct that has an id property to store their social security number:
+
+struct PersonId {
+    var id: String
+
+    init(id: String) {
+        self.id = id
+    }
+}
+
+let jude = PersonId(id: "12345")
+
+// Once that person has been created, we can make their id be private so you can’t read it from outside the struct – trying to write ed.id simply won’t work.
+
+// Just use the private keyword, like this:
+
+struct PersonWithPrivate {
+    private var id: String
+
+    init(id: String) {
+        self.id = id
+    }
+}
+
+let tryAccessIdDirectly = PersonWithPrivate(id: "12345")
+// tryAccessIdDirectly.id  => 'id' is inaccessible due to 'private' protection level
+
+// Now only methods inside Person can read the id property. For example:
+
+struct PersonWithMethod {
+    private var id: String
+
+    init(id: String) {
+        self.id = id
+    }
+
+    func identify() -> String {
+        return "My social security number is \(id)"
+    }
+}
+
+let tryAccessWithAFunc = PersonWithMethod(id: "12345")
+tryAccessWithAFunc.identify()
+
+// Another common option is public, which lets all other code use the property or method.
