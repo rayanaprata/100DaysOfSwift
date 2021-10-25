7735358283310ceda50300f385545654d05e5c21
Rayana Prata <50561896+rayanaprata@users.noreply.github.com>
2021-10-24 21:35:35 -0300

    structs summary

--- Day 9/day9.playground/Contents.swift
+++ Day 9/day9.playground/Contents.swift
@@ -168,3 +168,18 @@ let tryAccessWithAFunc = PersonWithMethod(id: "12345")
 tryAccessWithAFunc.identify()
 
 // Another common option is public, which lets all other code use the property or method.
+
+
+
+// structs summary
+
+//You’ve made it to the end of the seventh part of this series, so let’s summarize:
+
+// 1. You can create your own types using structures, which can have their own properties and methods.
+// 2. You can use stored properties or use computed properties to calculate values on the fly.
+// 3. If you want to change a property inside a method, you must mark it as mutating.
+// 4. Initializers are special methods that create structs. You get a memberwise initializer by default, but if you create your own you must give all properties a value.
+// 5. Use the self constant to refer to the current instance of a struct inside a method.
+// 6. The lazy keyword tells Swift to create properties only when they are first used.
+// 7. You can share properties and methods across all instances of a struct using the static keyword.
+// 8. Access control lets you restrict what code can use properties and methods.
