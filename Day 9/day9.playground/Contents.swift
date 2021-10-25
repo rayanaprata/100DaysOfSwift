a54ff7e764362b41205031b74dd5343fde12789f
Rayana Prata <50561896+rayanaprata@users.noreply.github.com>
2021-10-24 21:20:39 -0300

    referring to the current instance

--- Day 9/day9.playground/Contents.swift
+++ Day 9/day9.playground/Contents.swift
@@ -26,3 +26,23 @@ var userWithInit = UserWithInitializer()
 userWithInit.username
 
 
+
+// referring to the current instance
+
+// Inside methods you get a special constant called self, which points to whatever instance of the struct is currently being used.
+
+// This self value is particularly useful when you create >>> initializers that have the same parameter names <<< as your property.
+
+// For example, if you create a Person struct with a name property, then tried to write an initializer that accepted a name parameter, self helps you distinguish between the property and the parameter – self.name refers to the property, whereas name refers to the parameter.
+
+//Here’s that in code:
+
+struct Person {
+    var name: String
+    
+    init(name: String) {
+        print("\(name) was born!")
+    //  property  = parameter
+        self.name = name
+    }
+}
