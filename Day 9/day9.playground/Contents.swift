7e858ce31ee4080accb622a03558f4147c1f280e
Rayana Prata <50561896+rayanaprata@users.noreply.github.com>
2021-10-24 21:25:36 -0300

    lazy properties

--- Day 9/day9.playground/Contents.swift
+++ Day 9/day9.playground/Contents.swift
@@ -46,3 +46,34 @@ struct Person {
         self.name = name
     }
 }
+
+
+
+// lazy properties
+
+// As a performance optimization, Swift lets you create some properties only when they are needed. As an example, consider this FamilyTree struct – it doesn’t do much, but in theory creating a family tree for someone takes a long time:
+
+struct FamilyTree {
+    init() {
+        print("Creating family tree!")
+    }
+}
+
+// We might use that FamilyTree struct as a property inside a Person struct, like this:
+
+struct PersonWithLazyVar {
+    var name: String
+    lazy var familyTree = FamilyTree()
+
+    init(name: String) {
+        self.name = name
+    }
+}
+
+var ed = PersonWithLazyVar(name: "Ed")
+
+// But what if we didn’t always need the family tree for a particular person? If we add the lazy keyword to the familyTree property, then Swift will only create the FamilyTree struct when it’s first accessed
+
+// So, if you want to see the “Creating family tree!” message, you need to access the property at least once:
+
+ed.familyTree
