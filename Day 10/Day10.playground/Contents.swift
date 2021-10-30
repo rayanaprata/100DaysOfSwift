99f0a1d9e348a746d80465c303bfe7b71877db9c
Rayana Prata <50561896+rayanaprata@users.noreply.github.com>
2021-10-30 19:05:51 -0300

    overriding methods

--- Day 10/Day10.playground/Contents.swift
+++ Day 10/Day10.playground/Contents.swift
@@ -33,3 +33,36 @@ class Poodle: Dog {
 
 
 
+// overriding methods
+
+//Child classes can replace parent methods with their own implementations – a process known as overriding. Here’s a trivial Dog class with a makeNoise() method:
+
+class DogOverriding {
+    func makeNoise() {
+        print("Woof!")
+    }
+}
+
+
+//If we create a new Poodle class that inherits from Dog, it will inherit the makeNoise() method. So, this will print “Woof!”:
+
+//class Poodle: Dog {
+//}
+
+//let pop = Poodle()
+//pop.makeNoise()
+
+//Method overriding allows us to change the implementation of makeNoise() for the Poodle class.
+
+//Swift requires us to use override func rather than just func when overriding a method – it stops you from overriding a method by accident, and you’ll get an error if you try to override something that doesn’t exist on the parent class:
+
+class PoodleOverriding: DogOverriding {
+    override func makeNoise() {
+        print("Yip!")
+    }
+}
+
+let pop = PoodleOverriding()
+pop.makeNoise()
+
+//With that change, poppy.makeNoise() will print “Yip!” rather than “Woof!”.
