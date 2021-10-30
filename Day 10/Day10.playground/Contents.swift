9102586c193a13c8f804fe019c512f528348677b
Rayana Prata <50561896+rayanaprata@users.noreply.github.com>
2021-10-30 19:01:41 -0300

    class inheritance

--- Day 10/Day10.playground/Contents.swift
+++ Day 10/Day10.playground/Contents.swift
@@ -14,3 +14,22 @@ class Dog {
 
 // creating instances of that class
 let poppy = Dog(name: "Poppy", breed: "Poodle")
+
+
+
+// class inheritance
+
+//The second difference between classes and structs is that you can create a class based on an existing class – it inherits all the properties and methods of the original class, and can add its own on top.
+//
+//This is called class inheritance or subclassing, the class you inherit from is called the “parent” or “super” class, and the new class is called the “child” class.
+
+class Poodle: Dog {
+    init(name: String) {
+        super.init(name: name, breed: "Poodle")
+    }
+}
+
+// For safety reasons, Swift always makes you call super.init() from child classes – just in case the parent class does some important work when it’s created.
+
+
+
