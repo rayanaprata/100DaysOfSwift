6fcfb747f1f4897d333b2631cd3b5766e7c72f4b
Rayana Prata <50561896+rayanaprata@users.noreply.github.com>
2021-11-02 20:22:29 -0300

    deinitializers

--- Day 10/Day10.playground/Contents.swift
+++ Day 10/Day10.playground/Contents.swift
@@ -118,3 +118,28 @@ singerStructCopy.name = "Justin"
 
 print(singerStruct.name)
 print(singerStructCopy.name)
+
+
+
+// Deinitializers
+
+class Person {
+    var name = "John Doe"
+
+    init() {
+        print("\(name) is alive!")
+    }
+
+    func printGreeting() {
+        print("Hello, I'm \(name)")
+    }
+    
+    deinit {
+        print("\(name) is no more!")
+    }
+}
+
+for _ in 1...3 {
+    let person = Person()
+    person.printGreeting()
+}
