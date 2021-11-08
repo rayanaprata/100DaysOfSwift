de982d786841f51e67fa01a499435744e1dab8e4
Rayana Prata <50561896+rayanaprata@users.noreply.github.com>
2021-11-08 08:28:29 -0300

    extensions

--- Day 11/Day11.playground/Contents.swift
+++ Day 11/Day11.playground/Contents.swift
@@ -36,3 +36,23 @@ protocol HasVacation {
 
 protocol Employee: Payable, NeedsTraining, HasVacation { }
 
+
+
+// extensions
+
+extension Int {
+    func squared() -> Int {
+        return self * self
+    }
+}
+
+let number = 5
+number.squared()
+
+extension Int {
+    var isEven: Bool {
+        return self % 2 == 0
+    }
+}
+
+number.isEven
