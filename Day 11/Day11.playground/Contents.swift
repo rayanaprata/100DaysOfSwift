edb5a1e6e9c28cc342dd5721de0b2815522de3b4
Rayana Prata <50561896+rayanaprata@users.noreply.github.com>
2021-11-10 20:20:37 -0300

    protocol extensions

--- Day 11/Day11.playground/Contents.swift
+++ Day 11/Day11.playground/Contents.swift
@@ -56,3 +56,23 @@ extension Int {
 }
 
 number.isEven
+
+
+
+// protocol extensions
+
+let pythons = ["Eric", "Graham", "John", "Michael", "Terry", "Terry"]
+let beatles = Set(["John", "Paul", "George", "Ringo"])
+
+extension Collection {
+    func summarize() {
+        print("There are \(count) of us:")
+
+        for name in self {
+            print(name)
+        }
+    }
+}
+
+pythons.summarize()
+beatles.summarize()
