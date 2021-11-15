4bffcf76a47042c27f2b79957361aa87df67b6db
Rayana Prata <50561896+rayanaprata@users.noreply.github.com>
2021-11-14 21:24:17 -0300

    unwrapping optionals

--- Day 12/Day12.playground/Contents.swift
+++ Day 12/Day12.playground/Contents.swift
@@ -2,3 +2,15 @@ import UIKit
 
 // handling missing data
 var age: Int? = nil
+
+
+
+// unwrapping optionals
+var name: String? = nil
+
+if let unwrapped = name {
+    print("\(unwrapped.count) letters")
+} else {
+    print("Missing name.")
+}
+
