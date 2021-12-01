97c0f8de15812ad8a0be4031b54b4031ed177dad
Rayana Prata <50561896+rayanaprata@users.noreply.github.com>
2021-11-30 22:53:34 -0300

    types of data

--- Day 13/Day13.playground/Contents.swift
+++ Day 13/Day13.playground/Contents.swift
@@ -3,10 +3,50 @@ import UIKit
 // swift review, day one
 
 // variables and constants
-var name = "Rayana Prata"
-name = "Taylor Swift"
+var nameVar = "Rayana Prata"
+nameVar = "Taylor Swift"
 
 let birthdayDate = "03/08/2000"
 
+
 // types of data
+
+// string
+var name: String
+name = "Tim McGraw"
+
+// int
+var age: Int
+age = 25
+
+// double
+var latitude: Double
+latitude = 36.166667
+
+// float
+var longitude: Float
+longitude = -86.783333
+longitude = -186.783333
+longitude = -1286.783333
+longitude = -12386.783333
+longitude = -123486.783333
+longitude = -1234586.783333
+
+// boolean
+var stayOutTooLate: Bool
+stayOutTooLate = true
+
+var nothingInBrain: Bool
+nothingInBrain = true
+
+var missABeat: Bool
+missABeat = false
+
+
+// type annotations wisely
+var ageWisely = 25
+var longitudeWisely = -86.783333
+var nothingInBrainWisely = true
+
+
 
