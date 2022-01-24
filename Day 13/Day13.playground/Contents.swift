5559fa77242cb51344150e8fac16c770ae825958
Rayana Prata <50561896+rayanaprata@users.noreply.github.com>
2022-01-24 14:58:35 -0300

    conditional statements

--- Day 13/Day13.playground/Contents.swift
+++ Day 13/Day13.playground/Contents.swift
@@ -163,3 +163,25 @@ personDictionary["middle"]
 personDictionary["month"]
 
 
+
+// conditional statements
+var action: String
+var person = "hater"
+
+if person == "hater" {
+    action = "hate"
+} else if person == "player" {
+    action = "play"
+} else {
+    action = "cruise"
+}
+
+if stayOutTooLate && nothingInBrain {
+    action = "cruise"
+}
+
+if !stayOutTooLate && !nothingInBrain {
+    action = "cruise"
+}
+
+
