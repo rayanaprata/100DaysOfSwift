10f8b468fd57e5aa96e3cca9fb86c7558b5a4c5f
Rayana Prata <50561896+rayanaprata@users.noreply.github.com>
2022-02-06 22:13:21 -0300

    day 14

---
+++ Day 14/Day14.playground/Contents.swift
@@ -0,0 +1,48 @@
+import UIKit
+
+// functions
+
+func favoriteAlbum() {
+    print("My favorite is Fearless")
+}
+
+favoriteAlbum()
+
+
+func printAlbumRelease(name: String, year: Int) {
+    print("\(name) was released in \(year)")
+}
+
+printAlbumRelease(name: "Fearless", year: 2008)
+printAlbumRelease(name: "Speak Now", year: 2010)
+printAlbumRelease(name: "Red", year: 2012)
+
+func countLetters(in string: String) {
+    print("The string \(string) has \(string.count) letters.")
+}
+countLetters(in: "Hello")
+
+
+func albumIsTaylor(name: String) -> Bool {
+    if name == "Taylor Swift" { return true }
+    if name == "Fearless" { return true }
+    if name == "Speak Now" { return true }
+    if name == "Red" { return true }
+    if name == "1989" { return true }
+
+    return false
+}
+
+if albumIsTaylor(name: "Red") {
+    print("That's one of hers!")
+} else {
+    print("Who made that?!")
+}
+
+if albumIsTaylor(name: "Blue") {
+    print("That's one of hers!")
+} else {
+    print("Who made that?!")
+}
+
+
